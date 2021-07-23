be571f41139bf0e51094d33fbebd48a65bf7cb4d
Michele <micheleierniero@gmail.com>
2021-07-23 13:34:09 +0200

    palindroma, soluzione con funzione

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,42 +1,58 @@
-// Chiedere all’utente di inserire una parola
+// // Chiedere all’utente di inserire una parola
+
+// var parola = prompt("Inserisci la parola che preferisci:");
+
+// console.log(parola);
+
+
+// // Creare una funzione per capire se la parola inserita è palindroma
+
+// // divisione parola in lettere
+
+// var parolaDivisaInLettere = parola.split('');
+
+// console.log(parolaDivisaInLettere);
+
+
+// // inversione della parola divisa in lettere
+
+// var parolaDivisaInLettere = parolaDivisaInLettere.reverse();
+
+// console.log(parolaDivisaInLettere);
+
+
+// // riunione delle lettere della parola divisa e invertita
+
+// var parolaDivisaInLettere = parolaDivisaInLettere.join('');
+
+// console.log(parolaDivisaInLettere);
+
+
+// // condizione
+
+
+// if(parola == parolaDivisaInLettere){
+//     console.log('la parola è palindroma');
+// } else {
+//     console.log('la parola non è palindroma');
+// }
+
+
+
 
 var parola = prompt("Inserisci la parola che preferisci:");
 
-console.log(parola);
+var parolaPalindroma = inverso(parola);
 
-
-// Creare una funzione per capire se la parola inserita è palindroma
-
-// divisione parola in lettere
-
-var parolaDivisaInLettere = parola.split('');
-
-console.log(parolaDivisaInLettere);
-
-
-// inversione della parola divisa in lettere
-
-var parolaDivisaInLettere = parolaDivisaInLettere.reverse();
-
-console.log(parolaDivisaInLettere);
-
-
-// riunione delle lettere della parola divisa e invertita
-
-var parolaDivisaInLettere = parolaDivisaInLettere.join('');
-
-console.log(parolaDivisaInLettere);
-
-
-// condizione
-
-
-if(parola == parolaDivisaInLettere){
+if(parola == parolaPalindroma){
     console.log('la parola è palindroma');
-} else {
+  } 
+else {
     console.log('la parola non è palindroma');
 }
 
-
-
+function inverso (stringa) {
+var stringaInversa = stringa.split('').reverse().join('');  
+  return stringaInversa;
+}
     
